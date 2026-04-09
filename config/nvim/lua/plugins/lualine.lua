6ba895f8195c821e86dccc8ad90820877a5e14c7
flatherskevin <flatherskevin@gmail.com>
2026-04-09 08:42:02 -0400

    Customize lualine, disable tmux status bar, use basename for session names

---
+++ config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,45 @@
+local function runtime_path()
+  local buf_ft = vim.bo.filetype
+  if buf_ft == "python" then
+    local venv = os.getenv("VIRTUAL_ENV")
+    if not venv then
+      local root = vim.fn.getcwd()
+      local candidate = root .. "/.venv/bin/python"
+      if vim.fn.filereadable(candidate) == 1 then
+        venv = root .. "/.venv"
+      end
+    end
+    if venv then
+      return venv:gsub(vim.fn.getcwd() .. "/", "")
+    end
+  elseif vim.tbl_contains({ "javascript", "typescript", "javascriptreact", "typescriptreact" }, buf_ft) then
+    local root = vim.fn.getcwd()
+    if vim.fn.isdirectory(root .. "/node_modules") == 1 then
+      return "./node_modules"
+    end
+  end
+  return ""
+end
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        section_separators = "",
+        component_separators = "",
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = { "branch" },
+        lualine_c = {
+          { "diagnostics" },
+          { "filename", path = 1, symbols = { modified = " [+]", readonly = " [-]" } },
+        },
+        lualine_x = { runtime_path },
+        lualine_y = { "location" },
+        lualine_z = {},
+      },
+    },
+  },
+}
